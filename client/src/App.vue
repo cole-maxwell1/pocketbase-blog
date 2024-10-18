fbc0f30925b3841547d3ec69ebfda3e13de9290d
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2024-10-18 17:00:48 -0500

    Add RouterView transition

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,5 +7,17 @@ import { RouterView } from 'vue-router'
   <NavBarComponent
     v-if="!$route.path.includes('login') && !$route.path.includes('register')"
   />
-  <RouterView />
+  <RouterView v-slot="{ Component}">
+    <transition
+    mode="out-in"
+    enter-active-class="transition duration-200 ease-in"
+    enter-from-class="-translate-y-4 opacity-0"
+    enter-to-class="translate-y-0 opacity-100"
+    leave-active-class="transition duration-150 ease-out"
+    leave-from-class="translate-y-0 opacity-100"
+    leave-to-class="-translate-y-4 opacity-0"
+    >
+      <component :is="Component" />
+    </transition>
+  </RouterView>
 </template>
